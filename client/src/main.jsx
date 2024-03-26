8c9ef74e7829a6c1680f6d679621020e32a00299
joshhigg <136753578+joshhigg@users.noreply.github.com>
2024-03-26 13:59:58 -0600

    main.jsx routes and placeholder server typedefs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>Error, message comes from main.jsx</h1>,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />
+      }, {
+        path: '/basketball',
+        element: <Basketball />
+      }, {
+        path: '/football',
+        element: <Football />
+      }, {
+        path: '/hockey',
+        element: <Hockey />
+      }, {
+        path: '/baseball',
+        element: <Baseball />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const { ApolloServer } = require('@apollo/server');
 const cors = require('cors');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth')
 
-// const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
